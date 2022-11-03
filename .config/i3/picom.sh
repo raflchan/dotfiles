c5262680f4b6e04f9299c2c8b0a83b78a1c842e6
rafl <xrjhx@ymail.com>
2022-11-03 15:38:51 +0100

    improved backend selector

--- .config/i3/picom.sh
+++ .config/i3/picom.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
 driver=$(lspci -k | grep -A 2 -E "(VGA|3D)" | grep "Kernel modules: " | tr -d '\t' | sed -e 's/^Kernel modules: //')
+vendor=$(glxinfo | grep  "OpenGL vendor string:" | sed -e "s/^OpenGL vendor string: //g")
 echo "detected '$driver' as driver"
+echo "detected '$vendor' as vendor"
 
-case $driver in
-    "vmwgfx")
+case $vendor in
+    "Mesa/X.org")
         echo "falling back to xrender backend"
         picom --backend xrender
         exit
@@ -14,4 +16,3 @@ case $driver in
         exit
         ;;
 esac
-
